7d165d7b837997fe45d2e856b912f4a9ac7920d2
elahe dastan <elahe.dstn@gmail.com>
2021-01-23 22:48:21 +0330

    ends in normalizing

--- normalize/normalize.go
+++ normalize/normalize.go
@@ -32,12 +32,13 @@ func singleChar(words []string) []string {
 	return ans
 }
 
-func zeroWidthNonJoiner(words []string) []string {
+// ZWNJ and ZWSP
+func zeroWidth(words []string) []string {
 	ans := make([]string, 0)
 	for _, word := range words {
 		normalizedWord := make([]rune, 0)
 		for _, r := range []rune(word){
-			if r != '‌'{
+			if r != '‌' && r != '​' && r != '­'{
 				normalizedWord = append(normalizedWord, r)
 			}
 		}
@@ -47,8 +48,46 @@ func zeroWidthNonJoiner(words []string) []string {
 	return ans
 }
 
+func ha(words []string) []string {
+	ans := make([]string, 0)
+	for _, word := range words {
+		// ها
+		runes := []rune(word)
+		if string(runes[len(runes)-2:]) == "ها"{
+			if string(runes[:len(runes)-2]) != "تن"{
+				word = string(runes[:len(runes)-2])
+			}
+		}
+		if word != "" {
+			ans = append(ans, word)
+		}
+	}
+//	ends = ['ات', 'ان', 'ترین', 'تر', 'م', 'ت', 'ش', 'یی', 'ی', 'ها', 'ٔ', '‌ا',]
+//ترین
+	return ans
+}
 
+func tarin(words []string) []string {
+	ans := make([]string, 0)
+	for _, word := range words {
+		// ترین
+		runes := []rune(word)
+		if len(runes) < 4 {
+			continue
+		}
+		if string(runes[len(runes)-4:]) == "ترین"{
+			if string(runes[:len(runes)-4]) != "تن"{
+				word = string(runes[:len(runes)-4])
+			}
+		}
+		if word != "" {
+			ans = append(ans, word)
+		}
+	}
+	//	ends = ['ات', 'ان', 'ترین', 'تر', 'م', 'ت', 'ش', 'یی', 'ی', 'ها', 'ٔ', '‌ا',]
+	return ans
+}
 
 func Normalize(word string) []string {
-	return singleChar(number(zeroWidthNonJoiner(punctuation(word))))
+	return tarin(ha(singleChar(number(zeroWidth(punctuation(word))))))
 }
