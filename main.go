c4f330220966d45554929fec896f3ebd6652e7d5
elahe dastan <elahe.dstn@gmail.com>
2021-01-27 11:37:05 +0330

    add cluster docs

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	champion_list "shakhes/champion-list"
+	"fmt"
 	"shakhes/index"
 )
 
 func main() {
+	// main indexing
+	//i := index.NewIndex("./docs", 6)
+	//indexFile := i.Construct()
+	//
+	//c := champion_list.NewChampion(indexFile, 1)
+	//c.Create()
+
+	// cluster indexing
 	i := index.NewIndex("./docs", 6)
 	indexFile := i.Construct()
-
-	c := champion_list.NewChampion(indexFile, 1)
-	c.Create()
+	fmt.Println(indexFile)
 }
