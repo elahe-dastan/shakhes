d02d54bae5ffa806e8ac34949d4a5b73b9ab12ce
Elahe Dastan <elahe.dstn@gmail.com>
2021-01-31 18:41:47 +0330

    frequency of term

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 	champion_list "shakhes/champion-list"
 	"shakhes/index"
+	"shakhes/tokenize"
 )
 
 func main() {
 	//main indexing
 	i := index.NewIndex("./docs", 6, ".")
 	indexFile := i.Construct()
+
+	// kesafat and just becuase of time
+	f, err := os.Open(indexFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		termPostinglistFinal := tokenize.UnmarshalFinal(line)
+		fmt.Println(termPostinglistFinal)
+		//fmt.Println(termPostinglistFinal.Marshal())
+	}
+
 
 	c := champion_list.NewChampion(indexFile, 1)
 	c.Create()
